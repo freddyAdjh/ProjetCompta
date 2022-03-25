5f0f300f3257062a8063dd39f9d69fcf1fda7b68
freddyAdjh <frederic.adjewa237@gmail.com>
2022-03-25 16:49:09 +0100

    creation des classes permanentes et transitoires du système

--- StockCompta/admin.py
+++ StockCompta/admin.py
@@ -2,14 +2,37 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Article,Sortie
+from .models import Article,Sortie,Bill,personnel,price_Class,Provider
+
 
 @admin.register(Article)
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id','label','price','ActualQty','limitQty')
+    list_display = ('id','label','price','ActualQty','limitQty','AddedDate','idPrix')
 
 @admin.register(Sortie)
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('jour','mois','an','beneficiary','label','qte','service')
+    list_display = ('id','Date','qte','idArticle','idPersonnel')
+
+
+@admin.register(Bill)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','numero','date','idFournisseur','idUsername')
+
+@admin.register(Provider)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','label','code')
+
+
+@admin.register(price_Class)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','prix','date')
+
+@admin.register(personnel)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email','name','numero','service')
--- StockCompta/models.py
+++ StockCompta/models.py
@@ -3,21 +3,59 @@ from pickle import TRUE
 from pyexpat import model
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
+
+# class personnel
+
+class personnel(models.Model):
+    email = models.EmailField(primary_key=True)
+    name = models.CharField(max_length=50,default=None)
+    numero = models.IntegerField()
+    service = models.CharField(max_length=50,default=None)
+
+# class prix
+class price_Class(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    prix = models.IntegerField()
+    date = models.DateField()
+
+# class fournisseur
+class Provider(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    label = models.CharField(max_length=25)
+    code = models.CharField(max_length=30)
+
+
+# class Facture
+class Bill(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    numero = models.IntegerField()
+    date = models.DateField()
+    idFournisseur = models.ForeignKey(Provider,default=None,on_delete=models.CASCADE)
+    idUsername = models.ForeignKey(User,default=None,on_delete=models.CASCADE)
+
+
+# class article
+
 class Article(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
     label = models.CharField(max_length=30)
     price =  models.IntegerField()
     ActualQty = models.IntegerField()
     limitQty = models.IntegerField()
+    AddedDate = models.DateField()
+    idPrix = models.ForeignKey(price_Class,default=None,on_delete=models.CASCADE)
+
+    
+# class d'association sortie
 
 class Sortie(models.Model):
-    jour = models.CharField(max_length=15,default=datetime.now().day)
-    mois = models.CharField(max_length=15,default=datetime.now().month)
-    an = models.CharField(max_length=15,default=datetime.now().year)
-    beneficiary = models.CharField(max_length=50,null=True)
-    label = models.CharField(max_length=50,null=True)
+    id = models.AutoField(primary_key=True,null=False)
+    Date = models.DateField()
     qte = models.IntegerField()
-    service = models.CharField(max_length=50,null=True)
+    idArticle = models.ForeignKey(Article,default=None,on_delete=models.CASCADE)
+    idPersonnel = models.ForeignKey(personnel,default=None,on_delete=models.CASCADE)
 
 
     
